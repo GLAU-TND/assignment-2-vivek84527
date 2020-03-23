fc5417139eebcf06c9da9b56712156c26c436746
vivek84527 <viveksingh84527@gmail.com>
2020-03-23 23:21:21 +0530

    Object Of MyBinarySearchTree is created

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,6 +6,11 @@
  */
 package problem2.main;
 // executable class
+
+import problem1.mybst.MyBinarySearchTree;
+
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    MyBinarySearchTree bst = new MyBinarySearchTree();
+
 }
