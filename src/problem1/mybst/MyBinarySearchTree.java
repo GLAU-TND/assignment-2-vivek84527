ecc76296f956bdf2e6060130ace120e5f27c8724
vivek84527 <viveksingh84527@gmail.com>
2020-03-23 23:02:47 +0530

    Insertion in the binary tree

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,40 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+    public void insert(int data){
+        TreeNode node = new TreeNode(data);
+        if(root == null){
+            root=node;
+        }
+        else{
+            TreeNode temp=root;
+            TreeNode parent = null;
+            while(temp!=null){
+                parent=temp;
+                if(node.getData()<=temp.getData()){
+                    temp=temp.getLeft();
+                }
+                else{
+                    temp=temp.getRight();
+                }
+            }
+            if (node.getData()<=parent.getData()){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+        }
+
+
+    }
+
 }
